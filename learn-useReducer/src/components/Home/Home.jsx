3276868ce5f48a6b4a1af32a844f97b40268771c
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-11-10 21:45:56 +0530

    on logout

--- learn-useReducer/src/components/Home/Home.jsx
+++ learn-useReducer/src/components/Home/Home.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = ({onLogout}) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <button onClick={onLogout}>Logout</button>
     </Card>
   );
 };
